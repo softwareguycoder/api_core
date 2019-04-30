9a8a80d37cec02c8b8774b5c8939ab63fd62b626
softwareguycoder <softwareguycoder@gmail.com>
2019-04-30 09:05:46 -0400

    Added source and include files

---
+++ api_core/include/api_core.h
@@ -0,0 +1,18 @@
+/*
+ * api_core.h
+ *
+ *  Created on: Apr 30, 2019
+ *      Author: bhart
+ */
+
+#ifndef __API_CORE_H__
+#define __API_CORE_H__
+
+/**
+ * @brief Boolean value data type.
+ */
+typedef enum {
+	FALSE, TRUE
+} BOOL;
+
+#endif /* __API_CORE_H__ */
---
+++ api_core/include/stdafx.h
@@ -0,0 +1,10 @@
+// stdafx.h - Precompiled header
+
+#ifndef __API_CORE_STDAFX_H__
+#define __API_CORE_STDAFX_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#endif /* __API_CORE_STDAFX_H__ */
